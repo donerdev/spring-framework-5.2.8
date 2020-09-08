701ab9eb05a663ad0fa63cd0fbfbc91b7b19131d
linxu <linxu@csii.com.cn>
2020-09-08 20:13:03 +0800

    init

--- test/src/main/java/IocTest.java
+++ test/src/main/java/IocTest.java
@@ -6,6 +6,5 @@ public class IocTest {
 		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext("pojo");
 		Test test = applicationContext.getBean(Test.class);
 		test.test();
-		}
-//https://github.com/spring-projects/spring-framework/issues/25611  Process 'command 'git'' finished with non-zero exit value 128Process 'command 'git'' finished with non-zero exit value 128
+	}
 }
